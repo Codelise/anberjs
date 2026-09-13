8533e8a4e68aad40026a88241f4bb94e50ff5369
Codelise <claycaluscos@gmail.com>
2026-09-13 22:27:23 +0800

    Added Pause and Play

--- src/music/music.js
+++ src/music/music.js
@@ -1,27 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // import music file input
+  const importFileInput = document.createElement("input");
+  importFileInput.type = "file";
+  importFileInput.accept = "audio/*";
+  importFileInput.setAttribute("id", "audioFileInput");
+
+  // <audio> attributes
+  const audioPlayer = document.createElement("audio");
+  audioPlayer.setAttribute("id", "audioPlayer");
+  audioPlayer.controls = false; // hides controls
+
+  // X Button
   const importBtn = document.querySelector("#btn-x");
   importBtn.addEventListener("click", () => {
-    const importFileInput = document.createElement("input");
-    importFileInput.type = "file";
-    importFileInput.accept = "audio/*";
-    importFileInput.setAttribute("id", "audioFileInput");
+    importFileInput.click(); // automatically opens file manager
+    importPlayMusic();
+  });
 
-    const audioPlayer = document.createElement("audio");
-    audioPlayer.setAttribute("id", "audioPlayer");
-    audioPlayer.controls = true;
+  // A Button Pause
+  const pauseBtn = document.querySelector("#btn-a");
+  pauseBtn.addEventListener("click", () => {
+    pausePlayMusic();
+  });
 
-    document.body.appendChild(importFileInput);
-    document.body.appendChild(audioPlayer);
-
+  // FUNCTIONS:
+  // import music and play music
+  const importPlayMusic = () => {
     importFileInput.addEventListener("change", (event) => {
       const importedFile = event.target.files[0];
       if (importedFile) {
         const audioURL = URL.createObjectURL(importedFile);
-        console.log(audioURL);
-        const player = document.getElementById("audioPlayer");
-        player.src = audioURL;
-        player.play();
+        audioPlayer.src = audioURL;
+        audioPlayer.play();
       }
     });
-  });
+  };
+
+  // Pause or Play Music
+  const pausePlayMusic = () => {
+    if (audioPlayer.paused) {
+      audioPlayer.play();
+    } else {
+      audioPlayer.pause();
+    }
+  };
 });
+
+// TO DO:
+// display Song info
+// Add imported music to list and localStorage/database
+// SKIP music
